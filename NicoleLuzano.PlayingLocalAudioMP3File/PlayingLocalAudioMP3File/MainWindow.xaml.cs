a55b6ccb367927f8d830b122d516161536575c4a
DESKTOP-D26876I\Nicole <nmluzano@csmbataan.ph.education>
2021-02-08 18:31:53 +0800

    Controlling the MediaPlayer

--- NicoleLuzano.PlayingLocalAudioMP3File/PlayingLocalAudioMP3File/MainWindow.xaml.cs
+++ NicoleLuzano.PlayingLocalAudioMP3File/PlayingLocalAudioMP3File/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Microsoft.Win32;
+
 
 namespace PlayingLocalAudioMP3File
 {
@@ -23,20 +25,44 @@ namespace PlayingLocalAudioMP3File
     {
         private MediaPlayer mediaPlayer = new MediaPlayer();
 
+
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void btnOpenAudioFile_Click(object sender, RoutedEventArgs e)
-        {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
             if (openFileDialog.ShowDialog() == true)
-            {
                 mediaPlayer.Open(new Uri(openFileDialog.FileName));
-                mediaPlayer.Play();
-            }
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (mediaPlayer.Source != null)
+                lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+            else
+                lblStatus.Content = "No file selected...";
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
         }
     }
 }
