6f38a99d0421378495a91f1dc2268541f5ec745c
DESKTOP-D26876I\Nicole <nmluzano@csmbataan.ph.education>
2021-02-10 16:59:34 +0800

    Added open button and added code

--- NicoleLuzano.PlayingLocalAudioMP3File/PlayingLocalAudioMP3File/MainWindow.xaml.cs
+++ NicoleLuzano.PlayingLocalAudioMP3File/PlayingLocalAudioMP3File/MainWindow.xaml.cs
@@ -44,8 +44,14 @@ namespace PlayingLocalAudioMP3File
         
         void timer_Tick(object sender, EventArgs e)
         {
-            if (mediaPlayer.Source != null)
+            if (mediaPlayer.Source != null) 
+            { 
+                if (mediaPlayer.NaturalDuration.HasTimeSpan) 
+                { 
                 lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+                }
+            }
+                
             else
                 lblStatus.Content = "No file selected...";
         }
@@ -63,6 +69,17 @@ namespace PlayingLocalAudioMP3File
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             mediaPlayer.Stop();
+        }
+
+        private void btnOpenAudioFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                mediaPlayer.Play();
+            }
         }
     }
 }
